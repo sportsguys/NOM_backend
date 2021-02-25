190e5b25824e120c1f90ebb5054143b7a1b87035
max <maximumbalk@gmail.com>
2021-02-24 23:48:04 -0600

    remove placeholder module. app factory can debug

--- server/app.py
+++ server/app.py
@@ -1,15 +1,12 @@
 from flask import Flask
 import server.config as conf
 
-def start():
+def create_app():
     app = Flask(__name__)
     app.config.from_object(conf.dev)
-
-    from server.module1 import mod1
-    app.register_blueprint(mod1.bp, url_prefix='/route1')
     
     from server.player import routes as player_routes
     app.register_blueprint(player_routes.bp, url_prefix='/player')
 
     
-    app.run()
+    return app
