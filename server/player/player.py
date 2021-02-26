8d5180476ae88b8609c600643f92f7c6b8af0d90
max <maximumbalk@gmail.com>
2021-02-26 01:16:29 -0600

    better distance from k score. debug info in readme

--- server/player/player.py
+++ server/player/player.py
@@ -2,39 +2,46 @@ from server.player.minisom import MiniSom
 import numpy as np
 import pickle
 
-def _gen_test_data():
-    """ Randomly generate probability table because we a need non-uniform random distribution.
-
-        Create samples and assign label that follows pattern in samples.
-
-        Normalize all feature dimensions to [0,1].
-
-        Returns tuple of lists data, labels
-    """
-    p=np.random.randint(low=1, high=10, size=(1,50))
-    p = p / p.sum(axis=1, keepdims=1)
-    data = []
-    labels = []
-    for i in range (100):
-        sample = np.random.choice(50, 10, replace=False, p=p[0])
-        data.append(sample)
-        labels.append(np.sum(sample))
-    data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
-    return data, labels
-
 def _get_indices_of_k_smallest(arr, k):
-    """ Returns tuple of lists of indicies in the form:
-        ([x0, x1, .. ,xk], [y0, y1, .. , yk])
+    """ Returns tuple of lists of indicies in the form
+        ([x0, x1, .. ,xk], [y0, y1, .. , yk])\n
+        More dimensions add lists to the tuple ie ([xk],[yk],[zk])
     """
     return tuple(np.array(np.unravel_index(np.argpartition(arr.ravel(), k), 
         arr.shape))[:, range(min(k, 0), max(k, 0))])
-
 class PlayerModel():
     def __init__(self, name='test', data=None, labels=None):
         self.name = name
-        if data is None:
-            self.data, self.labels = _gen_test_data()
         self.som = None
+        self.dataset = data # not yet normalized for training
+        self.labels = labels
+
+    #TODO add percentile normalization,
+    #
+    def gen_test_data(self):
+        """ Randomly generate probability table because we a need non-uniform random distribution.
+
+            Create samples and assign label that follows pattern in samples.
+
+            Returns tuple of lists data, labels
+        """
+        p=np.random.randint(low=1, high=10, size=(1,50))
+        p = p / p.sum(axis=1, keepdims=1)
+        data = []
+        labels = []
+        for i in range (100):
+            sample = np.random.choice(50, 10, replace=False, p=p[0])
+            data.append(sample)
+            labels.append(np.sum(sample))
+        self.dataset, self.labels = data, labels
+
+    def value_normalize(self):
+        """ normalize feature vectors in the dataset to contain 
+            values [0,1] based on mean and standard deviation
+            
+            self.dataset and self.data are separate to allow for other normalization techniques 
+        """
+        self.data = (self.dataset - np.mean(self.dataset, axis=0)) / np.std(self.dataset, axis=0)
 
     def train_model(self):
         n_features = len(self.data[0])
@@ -85,19 +92,18 @@ class PlayerModel():
         return winners
 
     def score_dist_from_greatest_k(self, k=5):
-        """ Assign scores to samples based on average distance from neurons containting the 
-            k greatest scores.
+        """ Assign scores to samples based on distance from neurons containting the 
+            k greatest scores. 
 
             Return list of scores with the same shape as and corresponding to data\n
-            Lower is better
         """
         scores = np.zeros(len(self.data))
         lm = self.som.labels_map(self.data, self.labels)
         winners = self._k_greatest_neurons(lm, k)
         for i, sample in enumerate(self.data):
             am = self.som.activate(sample)
-            score = 0
             for neuron, _ in winners:
-                scores[i] += np.linalg.norm(sample - am[neuron[0]][neuron[1]])
-            scores[i] /= k
-        return scores.tolist()
+                # neuron is a centroid coordinate
+                scores[i] += (1 / am[neuron[0]][neuron[1]]) 
+                # lower activation value = closer to neuron. using 1 might be arbitrary
+        return scores
